5b30d00d3741ad1582f0c130cd1b70bfe465cc1e
Navedul Hasan <navedmalik092@gmail.com>
2024-12-21 22:56:57 +0530

    add setter and getter

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    // Step 5: Getter and Setter methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
